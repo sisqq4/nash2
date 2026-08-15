6812f85baf10f9a5d598de84eb5f3bccaa578aa0
sisqq4 <947025240@qq.com>
2026-08-15 21:22:57 +0800

    删除测试代码

--- v1/outputs/stage2_v2_p1_batch48/A2_stage1_seed_20260713/analysis/deterministic_generalization_report_source.sql
+++
@@ -1,5 +0,0 @@
-SELECT * FROM generalization_summary;
-
-SELECT * FROM generalization_scenario_comparison ORDER BY scenario_order, series_order;
-
-SELECT * FROM generalization_comparisons ORDER BY comparison_order;
